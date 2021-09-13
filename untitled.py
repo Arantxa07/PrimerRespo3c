d01ae63e5e3c281b1e388816a0e7112ec40913c6
Arantxa07 <90258613+Arantxa07@users.noreply.github.com>
2021-09-13 12:25:40 -0500

    salvando mi trabajo de binder

---
+++ untitled.py
@@ -0,0 +1,14 @@
+#Este es el ejercicio del grupo tercero c
+#del día 13 de septiembre del 2021
+
+#esta linea coloca un mensaje en la consola
+print ("Este programa hace operacione básicas")
+num1 = 130
+numero_2 = 345
+suma - num1 + numero_2 # La resta se hace con el simbolo '-'
+multip = num1 * numero_2 # La division se hace con una '/'
+
+print("la suma es: ")
+print(suma)
+print("La multiplicacion da como resultado ",multip)
+
