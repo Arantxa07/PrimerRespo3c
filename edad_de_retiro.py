bc6a6c02653ccbb760592a8b3cec40b0cf7a50d1
ARANTXA XIMENA XIMENA <90258613+Arantxa07@users.noreply.github.com>
2021-10-06 12:30:44 +0000

    programa, edad de retiro

---
+++ edad_de_retiro.py
@@ -0,0 +1,21 @@
+#Este programa calcula la edad y año de retiro de una persona, dadas su edad actual y la edad a la que se quiere retirar
+#04 de octubre de 2021
+# 1. Inicio
+# 2. Definir año_actual = 2021
+año_actual = 2021
+# 3. Preguntar al usuario "Que edad tienes?"
+print("¿Que edad tienes?")
+# 4. Definir edad_actual = entrada del usuario
+edad_actual = int(input()) # int () convierte texto en numero entero, es decir sin decimales
+# 5. Preguntar al usuario "¿A que edad te quieres retirar?"
+print("¿A que edad te quieres retirar")
+# 6. Definir edad_de_retiro - entrada del usuario
+edad_de_retiro - int(input())
+# 7. Hacer años_para_retiro = edad_actual - edad_actual
+años_de_retiro = edad_de_retiro - edad_actual
+# 8. Hacer año_de_retiro = año actual + años para retiro
+año_de_retiro = año_actual + años_para_retiro
+# 9. Mostrar mensaje "Es el año" año_actual " hasta que te puedas retirar"
+print("Tienes",años_para_retiro, "años hasta que te puedas retirar")
+# 10. Mostrar mensaje "Es el año año_actual" entonces te podras retirar en" año_de_retiro
+print("Es el año", año_actual, "entonces, te podras retirar en", año_de_retiro)
